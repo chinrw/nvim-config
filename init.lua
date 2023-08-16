3a24cf7619b13803c36cf66d77e538b6933ff024
Ruowen Qin <chinqrw@gmail.com>
2023-08-16 15:08:27 -0500

    lsp/clangd: update clangd config

--- init.lua
+++ init.lua
@@ -1,13 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
 local uv = require("luv")
 local cpus = uv.available_parallelism()
 
 require("lspconfig").clangd.setup({
-  on_attach = on_attach,
-  capabilities = cmp_nvim_lsp.default_capabilities(),
 
   cmd = {
     "clangd",
