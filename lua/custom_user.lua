3040230daaa9104a05c216a5ee569ec77e82c7ca
Ruowen Qin <ruowenq2@illinois.edu>
2023-10-06 10:11:49 -0500

    update rust-analyzer path

--- lua/custom_user.lua
+++ lua/custom_user.lua
@@ -1,6 +1,6 @@
 return {
   require("lspconfig").rust_analyzer.setup({
-    cmd = { "/bin/rust-analyzer" },
+    cmd = { "/opt/homebrew/bin/rust-analyzer" },
   }),
   function()
     vim.g.instant_username = vim.env.USER
