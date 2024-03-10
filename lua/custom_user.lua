c4a79acf8a52f3f8e3056cf8018b0a14e7373e83
Ruowen Qin <chinqrw@gmail.com>
2024-03-10 22:26:11 +0800

    config/lsp: fix the nvim_buf_set_extmark errors for inlay hints
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- lua/custom_user.lua
+++ lua/custom_user.lua
@@ -1,8 +1,8 @@
 return {
-  require("lspconfig").rust_analyzer.setup({
-    cmd = { "/bin/rust-analyzer" },
-  }),
-  function()
-    vim.g.instant_username = vim.env.USER
-  end,
+  -- require("lspconfig").rust_analyzer.setup({
+  --   cmd = { "/bin/rust-analyzer" },
+  -- }),
+  -- function()
+  --   vim.g.instant_username = vim.env.USER
+  -- end,
 }
