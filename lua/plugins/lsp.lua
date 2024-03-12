9fff1a209d00e6bb1269b730ff015c60d1a1245e
Ruowen Qin <chinqrw@gmail.com>
2024-03-12 11:16:46 +0800

    config/lsp: disable LuaSnip, use native snip
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -81,12 +81,12 @@ return {
   },
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   keys = function()
+  --     return {}
+  --   end,
+  -- },
   {
     "danymat/neogen",
     dependencies = "nvim-treesitter/nvim-treesitter",
