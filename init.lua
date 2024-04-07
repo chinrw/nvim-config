a50115f1b834450d07f9c1f715855e1ffa57e6a6
Ruowen Qin <chinqrw@gmail.com>
2024-04-07 16:59:42 +0800

    config/init: comment unused clangd config
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- init.lua
+++ init.lua
@@ -3,25 +3,25 @@ require("config.lazy")
 
 -- vim.opt.rtp:append("/usr/share/vim/vimfiles")
 
-local uv = require("luv")
-local cpus = uv.available_parallelism()
-require("lspconfig").clangd.setup({
-  cmd = {
-    "clangd",
-    "--background-index",
-    "--fallback-style=google",
-    "-j=" .. cpus,
-    "--all-scopes-completion",
-    "--pch-storage=memory",
-    "--log=error",
-    "--completion-style=detailed",
-    "--header-insertion=iwyu",
-    "--header-insertion-decorators",
-    "--enable-config",
-    "--offset-encoding=utf-16",
-    "--ranking-model=heuristics",
-  },
-})
+-- local uv = require("luv")
+-- local cpus = uv.available_parallelism()
+-- require("lspconfig").clangd.setup({
+--   cmd = {
+--     "clangd",
+--     "--background-index",
+--     "--fallback-style=google",
+--     "-j=" .. cpus,
+--     "--all-scopes-completion",
+--     "--pch-storage=memory",
+--     "--log=error",
+--     "--completion-style=detailed",
+--     "--header-insertion=iwyu",
+--     "--header-insertion-decorators",
+--     "--enable-config",
+--     "--offset-encoding=utf-16",
+--     "--ranking-model=heuristics",
+--   },
+-- })
 
 require("lsp-lens").setup({
   enable = false,
