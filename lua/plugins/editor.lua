ee2931f9cb846089f199567980e801ed2867cfeb
Ruowen Qin <chinqrw@gmail.com>
2024-04-16 21:43:09 +0800

    config/plugins: add plugin for yazi fm
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,4 +7,20 @@ return {
     { "<leader>gf", "<cmd>Telescope git_bcommits<CR>", desc = "buffer commits" },
     },
   },
+  {
+  "mikavilpas/yazi.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>gy",
+      function()
+        require("yazi").yazi()
+      end,
+      { desc = "yazi fm" },
+    },
+  },
+  }
 }
