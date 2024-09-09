82dd97978966122a8454dce23629049e1bb4cfd4
Ruowen Qin <ruqin@redhat.com>
2024-09-09 16:53:32 +0800

    config/keymaps: use `gw` for hard formatting
    Signed-off-by: Ruowen Qin <ruqin@redhat.com>

--- lua/plugins/jump.lua
+++ lua/plugins/jump.lua
@@ -1,21 +1,5 @@
 return {
   {
-    "phaazon/hop.nvim",
-    event = "VeryLazy",
-    config = function()
-      local status_ok, hop = pcall(require, "hop")
-      if not status_ok then
-        return
-      end
-      hop.setup()
-      vim.keymap.set("", "<Leader>k", function()
-        hop.hint_words()
-      end, { remap = true })
-
-      hop.setup({ keys = "etovxqpdgfblzhckisuran;" })
-    end,
-  },
-  {
     "ggandor/flit.nvim",
     enabled = false,
   },
