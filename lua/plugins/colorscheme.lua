2e64d5db492f1afda29efd3ea985386179e23dfe
Ruowen Qin <chinqrw@gmail.com>
2024-11-13 11:29:30 +0800

    config/colorscheme: add ambulance for error icon
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -43,6 +43,14 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
+      icons = {
+        diagnostics = {
+          Error = " ",
+          Warn = " ",
+          Hint = " ",
+          Info = " ",
+        },
+      },
     },
   },
 }
