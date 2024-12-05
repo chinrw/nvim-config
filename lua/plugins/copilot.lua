0c1a942cd640df413d690f0eebb0caacca1551f4
Ruowen Qin <chinqrw@gmail.com>
2024-12-05 12:49:08 +0800

    config/copilot: remove the default plugin
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,12 @@
 return {
-  -- copilot
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-    },
-  },
+  -- -- copilot
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   opts = {
+  --     suggestion = { enabled = false },
+  --     panel = { enabled = false },
+  --   },
+  -- },
 }
