d330b1b9ce08c94c214d4b6f2998e5e293d36d86
Ruowen Qin <chinqrw@gmail.com>
2024-12-16 17:59:54 +0800

    config/plugins: remove the unused plugin
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,4 @@ return {
 
   { import = 'plugins.lsp' },
 
-  -- Telescope live_grep -> <Tab> to select files -> <C-q> to populate qflist -> <leader>h to find/replace in qf files
-  'gabrielpoca/replacer.nvim',
-
 }
