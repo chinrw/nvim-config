9fdde0cbc6b9e3802e5f2be0b0d7b611d3cbc4d1
Ruowen Qin <chinqrw@gmail.com>
2025-03-28 16:34:58 +0800

    config/lsp: add virtualLines for diagnostic
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -114,13 +114,17 @@ return {
       diagnostics = {
         underline = false,
         update_in_insert = true,
-        virtual_text = {
-          spacing = 2,
-          source = "if_many",
-          -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-          -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-          prefix = "icons",
+        virtual_lines = {
+          current_line = false,
         },
+        virtual_text = false,
+        -- virtual_text = {
+        --   spacing = 2,
+        --   source = "if_many",
+        --   -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+        --   -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+        --   prefix = "icons",
+        -- },
         severity_sort = true,
       },
     },
