cd40fc5630052e297652c6b0e68a9394d50fa1ea
Ruowen Qin <ruqin@redhat.com>
2025-05-07 22:10:55 +0800

    config/lsp: use autopep8 for python
    Signed-off-by: Ruowen Qin <ruqin@redhat.com>

--- lua/plugins/lsp/python.lua
+++ lua/plugins/lsp/python.lua
@@ -4,11 +4,14 @@ return {
     optional = true,
     opts = {
       formatters_by_ft = {
-        ["python"] = { "black", "isort" },
+        ["python"] = { "autopep8", "isort" },
       },
       formatters = {
-        black = {
-          prepend_args = { "-S", "--preview", "--enable-unstable-feature", "hug_parens_with_braces_and_square_brackets" },
+        -- black = {
+        --   prepend_args = { "-S", "--preview", "--enable-unstable-feature", "hug_parens_with_braces_and_square_brackets" },
+        -- },
+        autopep8 = {
+          prepend_args = { "--aggressive", "--aggressive" },
         },
       },
     },
