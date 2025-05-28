6aa349a29a228f8ee9995ef84ca54da246353bbf
Ruowen Qin <ruqin@redhat.com>
2025-05-28 17:19:53 +0800

    config/lsp: update python black formatter config
    Signed-off-by: Ruowen Qin <ruqin@redhat.com>

--- lua/plugins/lsp/python.lua
+++ lua/plugins/lsp/python.lua
@@ -4,15 +4,24 @@ return {
     optional = true,
     opts = {
       formatters_by_ft = {
-        ["python"] = { "autopep8", "isort" },
+        ["python"] = { "black", "isort" },
       },
       formatters = {
-        -- black = {
-        --   prepend_args = { "-S", "--preview", "--enable-unstable-feature", "hug_parens_with_braces_and_square_brackets" },
-        -- },
-        autopep8 = {
-          prepend_args = { "--aggressive", "--aggressive" },
+        black = {
+          prepend_args = {
+            "-S",
+            "--preview",
+            "--enable-unstable-feature",
+            "hug_parens_with_braces_and_square_brackets",
+            "--enable-unstable-feature",
+            "multiline_string_handling",
+            "--enable-unstable-feature",
+            "string_processing",
+          },
         },
+        -- autopep8 = {
+        --   prepend_args = { "--aggressive", "--aggressive" },
+        -- },
       },
     },
   },
