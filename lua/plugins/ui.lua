c7730e0f3a0a560323aebb6fd7a0095f416769e7
Ruowen Qin <chinqrw@gmail.com>
2025-08-27 14:18:12 +0800

    lua/plugins/ui.lua: fix for "bug: failed to run config for bufferline.nvim"
    issue: https://github.com/LazyVim/LazyVim/issues/6355
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -39,4 +39,14 @@ return {
       },
     },
   },
+  {
+    "catppuccin/nvim",
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
+  },
 }
