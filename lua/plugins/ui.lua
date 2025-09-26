774cb41b28e175a7957c41f57a1ec9904f291312
Ruowen Qin <chinqrw@gmail.com>
2025-09-26 13:42:26 +0800

    Revert "lua/plugins/ui.lua: fix for "bug: failed to run config for bufferline.nvim""
    This reverts commit c7730e0f3a0a560323aebb6fd7a0095f416769e7 as the
    original issue already fixed in upstream

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -39,14 +39,4 @@ return {
       },
     },
   },
-  {
-    "catppuccin/nvim",
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      return opts
-    end,
-  },
 }
